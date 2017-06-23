43770245d4ac384d25de9f4f14e08e1b3cd4fa4f
madhusankar <RajagopaRa01@CompanyNet.org>
2017-06-23 14:09:55 -0700

    Seperate apps

---
+++ apps/app_reglogin/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+    url(r'^$', views.index name ='my_index'),
+    url(r'^register$', views.registration name ='my_register'),
+    url(r'^login$', views.loginuser name ='my_loginuser')]
---
+++ apps/app_wall/views.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User, Message, Comment
+from django.core.urlresolvers import reverse
+
+def wallpage(request):
+    #Message.objects.all().delete()
+    if request.method == "POST":
+        if request.POST['action'] == "postmsg":
+            context = {
+                "name": request.session['first_name'],
+                "id" :request.session['user_id']
+                }
+            result = User.objects.addmsgs(request.POST,context)
+            print request.method
+            if not result['status']:
+                for error in result['errors']:
+                    messages.error(request,error)
+                    return redirect(reverse('wall_page'))
+            else: 
+                messages.success(request,"Successful")
+                return redirect(reverse('wall_page'))
+        else:
+            context = {
+                "name": request.session['first_name'],
+                "id" :request.session['user_id']
+                }
+            result = User.objects.addcomments(request.POST,context)
+            print request.method
+            if not result['status']:
+                for error in result['errors']:
+                    messages.error(request,error)
+                    return redirect(reverse('wall_page'))
+            else: 
+                messages.success(request,"Successful")
+                return redirect(reverse('wall_page'))
+
+            print "DIDNT ENTER"
+    else:
+            print "ENTERED GET"
+            context = {
+                    'blog' :  Message.objects.order_by('-id'),
+                    "name": request.session['first_name'],
+                    "usercomments" : Comment.objects.order_by('-id')
+                }
+    return render(request,'app_wall/wall.html', context)
+
+
+
+
+   
+
+
+
+
